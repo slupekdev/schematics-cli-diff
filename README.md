b41aa4e205de77a870c51da2c5691b0c01ef6051
Jacek Slupianek <slupek.dev@windowslive.com>
2020-11-14 21:35:44 +0100

    chore: init repository

---
+++ README.md
@@ -0,0 +1,25 @@
+# Schematics CLI diff
+
+This repository exposes a schematic generated with `schematics schematic --name sample` with each version of Schematic CLI.
+
+A dedicated branch per version makes changes very easy
+to watch. For example:
+
+* https://github.com/slupekdev/schematics-cli-diff/compare/0.0.1...0.0.2
+* https://github.com/slupekdev/schematics-cli-diff/compare/0.1002.0...0.1100.1
+
+See table below for the complete list.
+
+Note that this is inspired by [Angular CLI diff](https://github.com/cexbrayat/angular-cli-diff).
+
+## Version changes
+
+Version|Compare view|Stats
+----|----|----
+
+## Why this repository
+
+After an upgrade of Schematic CLI on your project, you'll have to keep the configuration or dependencies up-to-date, and it can be hard to keep track.
+
+This project lets you easily know what changed. For example, from
+[0.1002.0 to the 0.1100.1](https://github.com/slupekdev/schematics-cli-diff/compare/0.1002.0...0.1100.1).
---
+++ newVersion.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+set -e
+
+versions=$(npm view @angular-devkit/schematics-cli versions --json)
+
+versions=${versions//\"/}
+versions=${versions//\[/}
+versions=${versions//\]/}
+versions=${versions//\,/}
+
+versions=(${versions})
+
+blocklist=()
+
+lastVersion="0.0.1"
+rebaseNeeded=false
+
+for version in "${versions[@]}"
+do
+
+  if [[ " ${blocklist[@]} " =~ " ${version} " ]]
+  then
+    echo "Skipping blocklisted ${version}"
+    continue
+  fi
+
+  if [ `git branch --list ${version}` ] || [ `git branch --list --remote origin/${version}` ]
+  then
+    echo "${version} already generated."
+    git checkout ${version}
+    if [ ${rebaseNeeded} = true ]
+    then
+      git rebase --onto ${lastVersion} HEAD~ ${version} -X theirs
+      diffStat=`git --no-pager diff HEAD~ --shortstat`
+      git push origin ${version} -f
+      diffUrl="[${lastVersion}...${version}](https://github.com/slupekdev/schematics-cli-diff/compare/${lastVersion}...${version})"
+      git checkout master
+      # rewrite stats in README after rebase
+      sed -i.bak -e "/^${version}|/ d" README.md && rm README.md.bak
+      sed -i.bak -e 's/----|----|----/----|----|----\
+NEWLINE/g' README.md && rm README.md.bak
+      sed -i.bak -e "s@NEWLINE@${version}|${diffUrl}|${diffStat}@" README.md && rm README.md.bak
+      git commit -a --amend --no-edit
+      git checkout ${version}
+    fi
+    lastVersion=${version}
+    continue
+  fi
+
+  echo "Generate ${version}"
+  rebaseNeeded=true
+  git checkout -b ${version}
+  # delete schematic
+  rm -rf sample
+  # generate schematic with new CLI version
+  npx @angular-devkit/schematics-cli@${version} schematic --name sample
+  git add sample
+  git commit -am "chore: version ${version}"
+  diffStat=`git --no-pager diff HEAD~ --shortstat`
+  git push origin ${version} -f
+  git checkout master
+  diffUrl="[${lastVersion}...${version}](https://github.com/slupekdev/schematics-cli-diff/compare/${lastVersion}...${version})"
+  # insert a row in the version table of the README
+  sed -i.bak "/^${version}|/ d" README.md && rm README.md.bak
+  sed -i.bak 's/----|----|----/----|----|----\
+NEWLINE/g' README.md && rm README.md.bak
+  sed -i.bak "s@NEWLINE@${version}|${diffUrl}|${diffStat}@" README.md && rm README.md.bak
+  # commit
+  git commit -a --amend --no-edit
+  git checkout ${version}
+  lastVersion=${version}
+
+done
+
+git checkout master
+git push origin master -f
